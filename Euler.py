7bc27ed200f593e1484c1891bb7dd4ce4b27ee45
EasyStarPilot <EasyStarPilot@gmx.de>
2024-12-14 01:27:05 +0100

    Nicer Indenting

--- Euler.py
+++ Euler.py
@@ -8,38 +8,38 @@ import pandas as pd
 data = pd.DataFrame(columns=['t', 'x', 'y'])
 
 #Schieberegler
-m = 68.1    #Masse in kg
-C = 0.8     #Spezieller Luftwiederstandungskoeffizient ((0;2])
-A = 60      #Querschnittsfläche in cm³
-vel_x = 0.1 #Start X-Geschwindigkeit
-vel_y = 0.1 #Start Y-Geschwindigkeit
-d_t = 0.01   #Zeitschritt
-t_max = 100 #Maximale Zeit
+m = 68.1                                #Masse in kg
+C = 0.8                                 #Spezieller Luftwiederstandungskoeffizient ((0;2])
+A = 60                                  #Querschnittsfläche in cm²
+vel_x = 0.1                             #Start X-Geschwindigkeit
+vel_y = 0.1                             #Start Y-Geschwindigkeit
+d_t = 0.01                              #Zeitschritt
+t_max = 100                             #Maximale Zeit
 
 # Konstanten
-g = 9.81    #Gravitation
-S = 1.29    #Luftdichte
+g = 9.81                                #Gravitation
+S = 1.29                                #Luftdichte
 
 #Initialisierung
-x = 0       #Streke in x Richtung in m
-y = 0       #Streke in y Richtung in m
-t = 0       #Zeitzählvariable
-F = g*m     #Anziehung der Kraft der Erde 
-a_x = 0     #Besleunigung in x Richtung
-a_y = 0     #Besleunigung in y Richtung
-Besch_x = 0 
-Besch_y = 0 
-d_v_x = 0   #Delta Geschwindigkeit in x Richtung
-d_v_y = 0   #Delta Geschwindigkeit in y Richtung
-v_x = 0     #Geschwindigkeit in x Richtung
-v_y = 0     #Geschwindigkeit in y Richtung
-d_S_x = 0   #Delta S in x Richtung
-d_S_y = 0   #Delta S in Y Richtung
+x = 0                                   #Streke in x Richtung in m
+y = 0                                   #Streke in y Richtung in m
+t = 0                                   #Zeitzählvariable
+F = g*m                                 #Anziehung der Kraft der Erde 
+a_x = 0                                 #Besleunigung in x Richtung
+a_y = 0                                 #Besleunigung in y Richtung
+Besch_x = 0                             #Luftwiederstandskoeffizient in x Richtung
+Besch_y = 0                             #Luftwiederstandskoeffizient in y Richtung
+d_v_x = 0                               #Delta Geschwindigkeit in x Richtung
+d_v_y = 0                               #Delta Geschwindigkeit in y Richtung
+v_x = 0                                 #Geschwindigkeit in x Richtung
+v_y = 0                                 #Geschwindigkeit in y Richtung
+d_S_x = 0                               #Delta S in x Richtung
+d_S_y = 0                               #Delta S in Y Richtung
 
 #Bewegung
 while t < t_max:
 
-    F_Luft = 0.5*S*A*C #ohne die Geschwindigkeit
+    F_Luft = 0.5*S*A*C                  #ohne die Geschwindigkeit
 
     # Bewegung in x-Richtung
     Besch_x += F-((vel_x - g)*F_Luft)   #Beschleunigung in x Richtung
