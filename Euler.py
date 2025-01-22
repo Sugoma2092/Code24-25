796f734b6237b069ec332734f0fafb2e32ae261e
EasyStarPilot <easystarpilot@gmx.de>
2025-01-22 10:01:16 +0100

    Sim des wag Wurf

--- Euler.py
+++ Euler.py
@@ -27,14 +27,10 @@ x = 0                                   #Strecke in x Richtung in m
 y = 0                                   #Strecke in y Richtung in m
 t = 0                                   #Zeitzählvariablen
 F = g*m                                 #Anziehung der Kraft der Erde 
-a_x = 0                                 #Beschleunigung in x Richtung
 a_y = 0                                 #Beschleunigung in y Richtung
-Besch_x = 0                             #Luftwiederstandkoeffizient in x Richtung
 Besch_y = 0                             #Luftwiederstandkoeffizient in y Richtung
 d_v_x = 0                               #Delta Geschwindigkeit in x Richtung
 d_v_y = 0                               #Delta Geschwindigkeit in y Richtung
-v_x = vel_x                             #Geschwindigkeit in x Richtung
-v_y = vel_y                             #Geschwindigkeit in y Richtung
 d_S_x = 0                               #Delta S in x Richtung
 d_S_y = 0                               #Delta S in Y Richtung
 
@@ -42,17 +38,12 @@ d_S_y = 0                               #Delta S in Y Richtung
 while t < t_max:
 
     # Bewegung in x-Richtung
-    F_Luft_X = 0.5*S*A*C*v_x*v_x           
-    Besch_x += F - F_Luft_X             #Beschleunigung in x Richtung
-    a_x = Besch_x / m                   #a in x-Richtung
-    d_v_x= a_x * d_t                    #Berechnung von Delta V in x Richtung
-    v_x +=d_v_x                         #Berechnung der neuen Geschwindigkeit in x Richtung
     d_S_x = v_x * d_t                   #Berechnung von Delta S in x Richtung
     x+=d_S_x                            #Strecke in x Richtung
 
     # Bewegung in y-Richtung
-    F_Luft_Y = 0.5 * S * A * C * v_y * v_y     
-    Besch_y = F - F_Luft_Y - m * g  # Reset Besch_y in each iteration
+    F_Luft_Y = 0.5 * S * A * C ** v_y   
+    Besch_y = F - F_Luft_Y          # Reset Besch_y in each iteration
     a_y = Besch_y / m               # a in y Richtung
     d_v_y = a_y * d_t               # Berechnung von Delta V in y Richtung
     v_y += d_v_y                    # Berechnung der neuen Geschwindigkeit in y Richtung
