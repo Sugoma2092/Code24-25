2928e59b590f5fd5f79f199fbf8cc3e8c5bf2624
EasyStarPilot <EasyStarPilot@gmx.de>
2025-01-27 23:46:20 +0100

    Good Graphs

--- Euler.py
+++ Euler.py
@@ -3,6 +3,7 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
 
 
 #Initialisieren eines leeren DataFrames zum Speichern der Daten
@@ -10,7 +11,7 @@ data = pd.DataFrame(columns=['t', 'x', 'y'])
 
 #Definition physikalischer Konstanten und Anfangsbedingungen
 m = 2               #Masse (kg)
-C = 0.45            #Spezieller Luftwiederstandskoeffizient ((0;2])
+C = 0.45            #Spezieller Luftwiderstandskoeffizient ((0;2])
 A = 0.1963          #Querschnittsfläche (m²) Formel: 2*pi*r #A=0.1963 für r=2,5cm
 vel_x = 15          #X-Anfangsgeschwindigkeit (m/sec)
 vel_y = 0           #Y-Anfangsgeschwindigkeit (m/sec)
@@ -27,26 +28,28 @@ y = 0               #Strecke in Y-Richtung (m)
 F = g*m             #Anziehungskraft der Erde
 a_x = 0             #Beschleunigung in X-Richtung (m/s²)
 a_y = 0             #Beschleunigung in Y-Richtung (m/s²)
-F_Luft_x = 0        #Luftwiederstandskraft in X-Richtung
-F_Luft_y = 0        #Luftwiederstandskraft in Y-Richtung
-k = C*A*S/2         #Luftwiederstandskoeffizient ohne Velocity^2
+F_Luft_x = 0        #Luftwiderstandskraft in X-Richtung
+F_Luft_y = 0        #Luftwiderstandskraft in Y-Richtung
+k = C*A*S/2         #Luftwiderstandskoeffizient ohne Velocity^2
 t = 0               #Zeit (sec)
 X_Punkte = []       #Liste für X-Koordinaten für Matplotlib
 Y_Punkte = []       #Liste für Y-Koordinaten für Matplotlib
 t_list = []         #Liste für die Zeit
+vel_x_start = vel_x
+vel_y_start = vel_y
 
 #Berechnen der Trajektorie
 while t<= t_max:
 
     #Berechnen der Bewegung in X-Richtung
-    F_Luft_x = k * vel_x**2     #Berechnung der Luftwiederstandskraft in X-Richtung
+    F_Luft_x = k * vel_x**2     #Berechnung der Luftwiderstandskraft in X-Richtung
     a_x = (F - F_Luft_x) / m    #Berechnung der Beschleunigung in X-Richtung
     vel_x = a_x * d_t + vel_x   #Berechnung der Änderung der Geschwindigkeit in X-Richtung
     x = x + vel_x * d_t         #Berechnung der Strecke in X-Richtung
     X_Punkte.append(x)
 
     #Berechnen der Bewegung in Y-Richtung
-    F_Luft_y = k * vel_y**2     #Berechnung der Luftwiederstandskraft in Y-Richtung
+    F_Luft_y = k * vel_y**2     #Berechnung der Luftwiderstandskraft in Y-Richtung
     a_y = (F - F_Luft_y) / m    #Berechnung der Beschleunigung in Y-Richtung
     vel_y = a_y * d_t + vel_y   #Berechnung der Änderung der Geschwindigkeit in Y-Richtung
     y = y + vel_y * d_t         #Berechnung der Strecke in Y-Richtung
@@ -72,11 +75,40 @@ for i in range(len(Y_Punkte)):
 data.to_excel('Waagerechter_Wurf.ods', engine='odf', index=False)
 
 #Einrichten von Matplotlib für das Zeichnen der Trajektorie
-plt.xlabel('X-Richtung')
-plt.ylabel('Y-Richtung')
-plt.title('Waagerechter Wurf \n Endgeschwindigkeit: ' + "{:10.4f}".format(vel_y) + 'm/s')
+plt.title('Waagerechter Wurf mit Luftwiderstand')
+plt.xlabel('X-Richtung in Metern')
+plt.ylabel('Y-Richtung in Metern')
+plt.annotate('Abwurfhöhe:'+ "{:10.2f}".format(Y_Punkte[0]) + 'm', xy=(0, Y_Punkte[0]), xytext=(0, Y_Punkte[0]*2/3), 
+            bbox=dict(boxstyle="round", fc="0.8"), arrowprops=dict(arrowstyle="->", facecolor='blue'),)
+plt.annotate('Wurfweite:'+ "{:10.2f}".format(X_Punkte[len(X_Punkte)-1]) + 'm\n' + "{:10.2f}".format(vel_y) + 'm/s', xy=(X_Punkte[len(X_Punkte)-1],0), xytext=(X_Punkte[len(X_Punkte)-1]/3,0), 
+            bbox=dict(boxstyle="round", fc="0.8"), arrowprops=dict(arrowstyle="->", facecolor='red'),)
+
+#Legende
+red_line = mlines.Line2D([], [], color='red', marker='_', markersize=15, label='Trajektorie des Körpers')
+plt.legend('upper right',handles=[red_line])
+
+#Konstantenangabe
+textstr_konst = '\n'.join((
+    'Konstanten:\n'
+    r'Masse in kg=%.2f' % (m, ),
+    r'Luftdichte in kg/m²=%.2f' % (S, ),
+    r'Querschnittsfläche in m²=%.2f' % (A, ),
+    r'Spezieller Luftwiderstandskoeffizient=%.2f' % (t, ),))
+props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+plt.text(35, 17, textstr_konst, fontsize=14, verticalalignment='baseline', bbox=props)
+
+#Veränerungsausgabe
+textstr_konst = '\n'.join((
+    'Veränderungen:\n'
+    r'Horizontale Startgeschwindigkeit=%.3f' % (vel_x_start, ),
+    r'Vertikale Startgeschwindigkeit=%.2f' % (vel_y_start, ),
+    r'Horizontale Endgeschwindigkeit=%.2f' % (vel_x, ),
+    r'Vertikale Endgeschwindigkeit=%.3f' % (vel_y, ),
+    r'Falldauer in Sekunden=%.2f' % (t, ),))
+props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+plt.text(35, 22, textstr_konst, fontsize=14, verticalalignment='center', bbox=props)
 
 #Anzeigen des Trajektorie-Plots
-plt.plot(X_Punkte, Y_Punkte, 'b-')
+plt.plot(X_Punkte, Y_Punkte, 'r-')
 plt.grid(True)
 plt.show()
