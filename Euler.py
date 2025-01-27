d0511c44a30a6aed0d650f3842e411c83e501b1b
EasyStarPilot <EasyStarPilot@gmx.de>
2025-01-27 19:21:01 +0100

    moved the graphes so that they end on Y=0

--- Euler.py
+++ Euler.py
@@ -1,15 +1,8 @@
-#pip install odf 
-#pip install odfpy 
-#pip install pandas
-#pip install matplotlib
+#pip install odf odfpy pandas matplotlib
 
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#Einrichten von Matplotlib für das Zeichnen der Trajektorie
-plt.xlabel('X-Richtung')
-plt.ylabel('Y-Richtung')
-plt.title('Waagerechter Wurf')
 
 #Initialisieren eines leeren DataFrames zum Speichern der Daten
 data = pd.DataFrame(columns=['t', 'x', 'y'])
@@ -20,8 +13,8 @@ C = 0.45            #Spezieller Luftwiederstandskoeffizient ((0;2])
 A = 0.1963          #Querschnittsfläche (m²)
 vel_x = 10          #X-Anfangsgeschwindigkeit (m/sec)
 vel_y = 0           #Y-Anfangsgeschwindigkeit (m/sec)
-d_t = 0.1           #Zeitschritt (sec)
-t_max = 2           #Maximale Zeit (sec)
+d_t = 0.01          #Zeitschritt (sec)
+t_max = 2.5         #Maximale Zeit (sec)
 
 #Definition der Schwerkraft und der Luftdichte
 g = 9.81            #Schwerkraft (m/s²)
@@ -39,9 +32,10 @@ k = C*A*S/2         #Luftwiederstandskoeffizient ohne Velocity^2
 t = 0               #Zeit (sec)
 X_Punkte = []       #Liste für X-Koordinaten für Matplotlib
 Y_Punkte = []       #Liste für Y-Koordinaten für Matplotlib
+t_list = []         #Liste für die Zeit
 
 #Berechnen der Trajektorie
-while t <= t_max:
+while y <= 10:
 
     #Berechnen der Bewegung in X-Richtung
     F_Luft_x = k * vel_x**2     #Berechnung der Luftwiederstandskraft in X-Richtung
@@ -55,20 +49,33 @@ while t <= t_max:
     a_y = (F - F_Luft_y) / m    #Berechnung der Beschleunigung in Y-Richtung
     vel_y = a_y * d_t + vel_y   #Berechnung der Änderung der Geschwindigkeit in Y-Richtung
     y = y + vel_y * d_t         #Berechnung der Strecke in Y-Richtung
-    Y_Punkte.append(-y)
+    Y_Punkte.append(y)
+
+    t_list.append(t)
+
+    #Inkrementieren der Zeit
+    t += d_t
+
+#Verschiebung nach oben, sodass es auf Y = 0 endet
+hight = max(Y_Punkte)
+for i in range(len(Y_Punkte)):
+    Y_Punkte[i] = (-Y_Punkte[i] + hight)
     
     #Ausgeben der aktuellen Zeit, Position und Geschwindigkeit
-    print(f"Zeit:","{:10.4f}".format(t), "  x:", "{:10.4f}".format(x), "    y:", "{:10.4f}".format(-y))
+    print(f"Zeit:","{:10.4f}".format(t_list[i]), "  x:", "{:10.4f}".format(X_Punkte[i]), "    y:", "{:10.4f}".format(Y_Punkte[i]))
 
     #Hinzufügen der aktuellen Werte von t, x, y zum DataFrame
-    data = data._append({'t': t, 'x': x, 'y': -y}, ignore_index=True)
-    
-    #Inkrementieren der Zeit
-    t += d_t        
+    data = data._append({'t': t_list[i], 'x': X_Punkte[i], 'y': Y_Punkte[i]}, ignore_index=True)
 
 #Exportieren des DataFrames in eine .ods-Datei
 data.to_excel('Waagerechter_Wurf.ods', engine='odf', index=False)
 
-#Anzeigen des Trajektorien-Plots
+#Einrichten von Matplotlib für das Zeichnen der Trajektorie
+plt.xlabel('X-Richtung')
+plt.ylabel('Y-Richtung')
+plt.title('Waagerechter Wurf')
+
+#Anzeigen des Trajektorie-Plots
 plt.plot(X_Punkte, Y_Punkte, 'b-')
+plt.grid(True)
 plt.show()
